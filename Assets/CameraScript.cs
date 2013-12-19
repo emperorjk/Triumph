8f381031431ebaa5c2c24d1c12caea929b4643d1
Joey Koster <koster.joey@gmail.com>
2013-12-19 12:54:17 +0100

    Improved camera position

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	void Start () {
+		transform.position = GameObject.FindGameObjectWithTag("BackgroundStart").renderer.bounds.center - new Vector3(0,0,1);
+	}
+}
