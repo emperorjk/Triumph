0d9f50f0dcb4b35cab40da785ed34f088a256bd3
ronaldmeijboom <ronaldme@gmail.com>
2014-02-04 14:02:04 +0100

    Loot prefab

---
+++ Assets/Scripts/Misc/Loot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loot : MonoBehaviour 
+{
+    private int AmountTurnsDestroy { get; set; }
+    public int CurrentTurnAmount { get; set; }
+    public int AmountLoot { get; private set; }
+
+    void Awake()
+    {
+        AmountTurnsDestroy = 4;
+    }
+
+    /// <summary>
+    /// After a number of turns destroy the Loot.
+    /// </summary>
+    public void IncreaseTurn()
+    {
+        CurrentTurnAmount++;
+
+        if (CurrentTurnAmount >= AmountTurnsDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void SetLoot(int loot)
+    {
+        AmountLoot += loot;
+    }
+
+    public void PickUpLoot(Player player)
+    { 
+        player.IncreaseGoldBy(AmountLoot);
+        Destroy(this.gameObject);
+    }
+}
