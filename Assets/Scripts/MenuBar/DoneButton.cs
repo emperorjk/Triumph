7d924cd144fbd821fdac4abcb86ab1214d3face0
Joey Koster <koster.joey@gmail.com>
2014-02-08 11:44:35 +0100

    Revert "Done button fades in and out."
    This reverts commit 5431b61e3635ef44302b4f8beeb7cbf90ce9d884.

--- Assets/Scripts/MenuBar/DoneButton.cs
+++ Assets/Scripts/MenuBar/DoneButton.cs
@@ -6,17 +6,12 @@ using UnityEngine;
 
 public class DoneButton : MonoBehaviour
 {
-    private bool _needsFading = false;
-    private bool _fadeIn = true;
 
     void Start()
     {
         Vector3 centerWorldSpace = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 5));
         transform.position = new Vector3(centerWorldSpace.x, centerWorldSpace.y + 1, centerWorldSpace.z);
         EventHandler.register<OnSwipeAction>(SwipeDoneButton);
-        Color col = renderer.material.color;
-        col.a = 0f;
-        renderer.material.color = col;
     }
 
     void OnDestroy()
@@ -29,44 +24,20 @@ public class DoneButton : MonoBehaviour
         if(evt.SwipeUp)
         {
             GameManager.Instance.IsDoneButtonActive = !GameManager.Instance.IsDoneButtonActive;
-            this.renderer.enabled = true;
-            this.collider.enabled = true;
-            _needsFading = true;
-        }
-    }
-
-    private void Fade()
-    {
-        Color col = renderer.material.color;
-        col.a = Mathf.Lerp(renderer.material.color.a, (_fadeIn ? 1 : 0), Time.deltaTime * 4.5f);
-
-        if (!_fadeIn && col.a < 0.10f)
-        {
-            col.a = 0f;
-            _needsFading = false;
-            _fadeIn = true;
             this.renderer.enabled = GameManager.Instance.IsDoneButtonActive;
             this.collider.enabled = GameManager.Instance.IsDoneButtonActive;
         }
-        else if (_fadeIn && col.a > 0.95f)
-        {
-            _fadeIn = false;
-            _needsFading = false;
-            col.a = 1f;
-        }
-        renderer.material.color = col;
     }
 
     void Update()
     {
-        if(_needsFading) { Fade(); }
-
-        if (Input.GetKeyDown(KeyCode.T) && !GameManager.Instance.IsDoneButtonActive && !_needsFading)
+        if(Input.GetKeyDown(KeyCode.T) && !GameManager.Instance.IsDoneButtonActive)
         {
+            // Fake swipe in order to have the code in 1 place.
             SwipeDoneButton(new OnSwipeAction(false, false, true, false));
         }
 
-        if (Input.GetMouseButtonDown(0) && !_needsFading)
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit _touchBox;
@@ -75,7 +46,9 @@ public class DoneButton : MonoBehaviour
                 if (_touchBox.collider == this.collider)
                 {
                     GameManager.Instance.EndTurn();
-                    SwipeDoneButton(new OnSwipeAction(false, false, true, false));
+                    GameManager.Instance.IsDoneButtonActive = false;
+                    this.renderer.enabled = GameManager.Instance.IsDoneButtonActive;
+                    this.collider.enabled = GameManager.Instance.IsDoneButtonActive;
                 }
             }
         }
