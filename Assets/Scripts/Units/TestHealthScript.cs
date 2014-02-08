ae716f5740cb822a125f897d708ccde32fa138a6
Joey Koster <koster.joey@gmail.com>
2014-02-08 12:41:33 +0100

    Test healthbar
    - Added a healthbar to 1 unit for testing. The healthbar is calculated
    from the health remaining of the unit.
    - Healthbar stays on forever. (except when the unit is destroyed
    ofcourse.
    - This is just for showing the healthbar effect. No changes have been
    made to any of the prefabs.

---
+++ Assets/Scripts/Units/TestHealthScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHealthScript : MonoBehaviour {
+
+    private UnitGameObject unit;
+
+	// Use this for initialization
+	void Start () {
+        unit = transform.parent.GetComponent<UnitGameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.material.SetFloat("_Cutoff", 1 - (unit.UnitGame.CurrentHealth / 10f));
+	}
+}
